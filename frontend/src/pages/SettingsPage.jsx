f7a0c68a737297a202b7349d98cd35827b6906c5
Abhimanyu Singh <abhimanyusinghjnv1001@gmail.com>
2025-11-16 23:44:39 +0530

    login and signup page

---
+++ frontend/src/pages/SettingsPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SettingsPage = () => {
+  return (
+    <div>
+      <h1>settings</h1>
+    </div>
+  )
+}
+
+export default SettingsPage
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,7 @@
 import daisyui from 'daisyui'
 
 
-/** @type {import('tailwindcss').Config} */
+/**@type {import('tailwindcss').Config}*/
 export default {
   content: [
     "./index.html",
